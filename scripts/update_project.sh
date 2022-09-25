b5dcee9e61feaf71daa2291d5e84db96fd5dda4a
cds05197 <cds05197@naver.com>
2022-09-25 18:57:14 +0900

    CI/CD Please

---
+++ scripts/update_project.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /root
+git clone https://github.com/cds05197/django.git
+cd django
+\cp -rf config MTV common template requirements.txt /my-django
+cd /my-django
+
+sudo sed -i "/twisted-iocpsupport==1.0.2/d" /my-django/requirements.txt
+pip install -r requirements.txt
+
+sudo sed -i "s/DEBUG = .*/DEBUG = False/" /my-django/config/settings.py
+sudo sed -i "s/is_Production = .*/is_Production = False/" /my-django/config/settings.py
+sudo sed -i "s/DataBase_Write_Endpoint = .*/DataBase_Write_Endpoint = \"{Write_Endpoint}\"/" /my-django/config/settings.py
+sudo sed -i "s/DataBase_Read_Endpoint = .*/DataBase_Read_Endpoint = \"{Read_Endpoint}\"/" /my-django/config/settings.py
+sudo sed -i "s/USE_CACHE = .*/USE_CACHE = False/" /my-django/config/settings.py
+sudo sed -i "s/IS_HOME = .*/IS_HOME = False/" /my-django/config/settings.py
+sudo sed -i "s/ REDIS_HOST = .*/ REDIS_HOST = \"{REDIS_HOST}\"/" /my-django/config/settings.py
+sudo systemctl restart kgcha
