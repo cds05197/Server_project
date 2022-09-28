d1168408de23a7cbe80b1e8899ea8e0110e28a6f
cds05197 <cds05197@naver.com>
2022-09-28 10:45:19 +0900

    Test CICD

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-DEPLOYMENT = "Deploy Success2"
+DEPLOYMENT = "Deploy Test in KGCHA"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
